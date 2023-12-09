db8fc9369b7046266f3ec1f12b245ec839c685ed
LedSquare <prophet3334@gmail.com>
2023-12-09 16:33:44 +0300

    includes, layout and start page

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    
+    <nav>
+        <ul>
+            <li>Домой</li>
+            <li>Личный кабинет</li>
+            <li>Контакты</li>
+        </ul>
+    
+        <ul class="auth-ul">
+            <li>Войти</li>
+            <li>Регистрация</li>
+        </ul>
+    </nav>
+    
+</header>
---
+++ resources/views/start-page.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+<div class="start-page flex-c">
+
+    <div class="img-container">
+        <img src="/layout/images/main-logo-middle.jpeg" alt="asasd" class="image">
+    </div>
+
+    <h1>ОБЛИК</h1>
+    <button class="start-button">Начать</button>
+
+</div>
