7ec3225046f5dec9007ec3888755f2abe10c9ae0
leybkovich_a <leybkovich_a@lave.tech>
2024-02-18 14:31:47 +0300

    Dev: delete layout and change web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+/**
+ * Laravel ui routes
+ */
 Auth::routes();
-
-Route::get('/', function () {
-    return view('pages.home');
-});
