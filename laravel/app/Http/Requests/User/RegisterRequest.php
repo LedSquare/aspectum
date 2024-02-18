a2acbf5ffc7a05fb170daf14f602a5e8d4cd12ab
leybkovich_a <leybkovich_a@lave.tech>
2024-02-18 15:34:58 +0300

    feat: create RegisterRequest.php

---
+++ laravel/app/Http/Requests/User/RegisterRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'string', 'min:3'],
+            'email' => ['required' , 'email', 'string', 'unique:users'],
+            'firstname' => ['string', 'nullable'],
+            'lastname' => ['string', 'nullable'],
+            'fathername' => ['string', 'nullable'],
+            'gender' => ['string', 'nullable'],
+            'password' => ['string', 'min:6', 'required', 'confirmed'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            //TODO: Сделать сообщения на русском
+        ];
+    }
+}
