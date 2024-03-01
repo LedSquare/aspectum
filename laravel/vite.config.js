0cf3baac4606d672e9937ec647cf7d023988915b
LedSquare <prophet3334@gmail.com>
2024-03-01 12:17:13 +0300

    build(deps): update package-lock.json, package.json and vite.config.js

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
@@ -19,6 +20,8 @@ export default defineConfig({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
+
+        vue(),
     ],
 
 });
