e5146ea542054bb1f74c4f82449946f30395fd13
LedSquare <prophet3334@gmail.com>
2024-03-02 22:30:32 +0300

    update: 2 files and delete 2 files

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,7 +1,5 @@
 import {createApp} from "vue/dist/vue.esm-bundler.js";
-import Hello from "./src/components/Hello.vue";
 const app = createApp({});
 
-app.component('hello', Hello)
 app.mount("#app");
 
