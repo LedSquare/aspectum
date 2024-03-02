6388640da33e94da679685f31c1dde372fb3187e
LedSquare <prophet3334@gmail.com>
2024-03-02 23:14:30 +0300

    Dev: register form

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -5,26 +5,17 @@
     <div class="form-value">
         <form action="{{ route('login') }}" method="POST"> @csrf
             <h2>Авторизация</h2>
-            <div class="input-group">
-                <ion-icon name="mail-outline"></ion-icon>
-                <x-input id="email" type="email" name="email">Почта:</x-input>
-            </div>
-            <div class="input-group">
-                <ion-icon name="lock-closed-outline"></ion-icon>
-                <x-input id="password" type="password" name="password">Пароль:</x-input>
-            </div>
+                <x-auth.input id="email" type="email" name="email">Почта</x-auth.input>
+                <x-auth.input id="password" type="password" name="password">Пароль:</x-auth.input>
             <div class="forget">
                 <label for=""><input type="checkbox">Запомнить<a href="#">, забыли пароль?</a></label>
             </div>
             <button type="submit">Войти</button>
             <div class="register-box">
-                <p> <a href="#">Зарегистрироваться</a></p>
+                <p> <a href="{{ route('register') }}">Зарегистрироваться</a></p>
             </div>
         </form>
     </div>
 </div>
 
-
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 @endsection
--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -1,77 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="username" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="form-box">
+    <div class="form-value">
+        <form action="{{ route('register') }}" method="POST"> @csrf
+            <h2>Регистрация</h2>
+                <x-auth.input id="username" type="username" name="username">Логин:</x-auth.input>
+                <x-auth.input id="email" type="email" name="email">Почта:</x-auth.input>
+                <x-auth.input id="phone" type="phone" name="phone">Телефон:</x-auth.input>
+                <x-auth.input id="password" type="password" name="password">Пароль:</x-auth.input>
+                <x-auth.input id="firstname" type="firstname" name="firstname">Имя:</x-auth.input>
+                <x-auth.input id="lastname" type="lastname" name="lastname">Фамилия:</x-auth.input>
+                <x-auth.input id="fathername" type="fathername" name="fathername">Отчество:</x-auth.input>
+                <x-auth.input id="gender" type="gender" name="gender">Пол:</x-auth.input>
+            <button type="submit">Зарегистрироваться</button>
+        </form>
     </div>
 </div>
 @endsection
