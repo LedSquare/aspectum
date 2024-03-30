bbd56ac2ebb7246286a0fdcb5a95e111a2aceace
LedSquare <prophet3334@gmail.com>
2024-03-30 23:45:00 +0300

    Fea: Installing Inertia and config

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -11,15 +11,40 @@ export default defineConfig({
             usePolling: true
         },
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @import "./resources/scss/_variables.scss";
+                `,
+            },
+        },
+    },
     plugins: [
         laravel({
             input: [
-                'resources/scss/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // Плагин Vue перепишет URL-адреса ресурсов, когда они
+                    // будут использоваться в однофайловых компонентах,
+                    // чтобы указывать на веб-сервер Laravel. Установка этого
+                    // значения в `null` позволяет вместо этого плагину Laravel
+                    // переписывать URL-адреса ресурсов, чтобы они указывали на сервер Vite.
+                    base: null,
+
+                    // Плагин Vue будет анализировать абсолютные URL-адреса и рассматривать
+                    // их как абсолютные пути к файлам на диске. Установка этого значения в
+                    // `false` оставит абсолютные URL-адреса нетронутыми, чтобы они могли
+                    // ссылаться на ресурсы в папке public, как ожидается.
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 
 
