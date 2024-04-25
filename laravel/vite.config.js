aab92e7f311e8e0f2ba64eb0477cc7d9947164d7
LedSquare <prophet3334@gmail.com>
2024-04-25 18:18:01 +0300

    Fea: Change domain name

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3000,
+        hmr: {
+            host: 'aspect.local'
+        },
 
         watch: {
             usePolling: true
