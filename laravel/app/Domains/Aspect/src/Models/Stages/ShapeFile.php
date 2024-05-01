a78d3cd7f6992650e16cfd29778662d6485b013f
LedSquare <prophet3334@gmail.com>
2024-05-01 15:12:36 +0300

    Fea: ShapeFile model

--- laravel/app/Domains/Aspect/src/Models/Stages/ShapeFile.php
+++ laravel/app/Domains/Aspect/src/Models/Stages/ShapeFile.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class ShapeFile extends Model
 {
     use HasFactory;
+
+    protected $table = 'a_shape_files';
+
+    protected $fillable = [
+        'filename',
+        'extension',
+        'size',
+        'disk',
+        'path',
+    ];
+
+    protected $casts = [
+        'size' => 'integer',
+    ];
 }
