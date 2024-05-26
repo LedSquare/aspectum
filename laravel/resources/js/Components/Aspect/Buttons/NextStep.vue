0f7843b3cc5221563991ce0050a4d5e0e2af44c5
LedSquare <prophet3334@gmail.com>
2024-05-26 23:16:27 +0300

    Fea: Inertia. #Aspect NextStep button

---
+++ laravel/resources/js/Components/Aspect/Buttons/NextStep.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    aspect_id: Number,
+});
+
+</script>
+<template>
+    <div>
+        <Link :href="route('aspect.post', { aspect: aspect_id })" as="button" class="start-button">
+        Следующий шаг
+        </Link>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- laravel/resources/js/Pages/Aspect/Word.vue
+++ laravel/resources/js/Pages/Aspect/Word.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import NextStep from '../../Components/Aspect/Buttons/NextStep.vue';
 
 const props = defineProps({
     title: String,
     data: Array,
+    aspect_id: Number,
 });
 
 
@@ -11,15 +13,19 @@ const props = defineProps({
 <template>
 
     <Head :title="title" />
-
-    <div v-if="data !== null" v-for="word in data" :key="word.id">
+    {{ aspect_id }}
+    <div v-if="data !== 0" v-for="word in data" :key="word.id">
         <div>
-            Понятие: {{ word.name }}
+            {{ word.name }}
         </div>
     </div>
     <div v-else>
         Понятий нет в базе данных, либо произошла другая ошибка
     </div>
+    <div>
+        <NextStep :aspect_id="aspect_id"></NextStep>
+    </div>
 </template>
+
 
 <style lang="scss" scoped></style>
