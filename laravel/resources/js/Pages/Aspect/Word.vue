80023c5c3e096aae59261a19fadf629e9d52decd
LedSquare <prophet3334@gmail.com>
2024-05-28 23:07:20 +0300

    Fea: #Frontend - Update: Word - words box added

--- laravel/resources/js/Pages/Aspect/Word.vue
+++ laravel/resources/js/Pages/Aspect/Word.vue
@@ -14,18 +14,28 @@ const props = defineProps({
 
     <Head :title="title" />
     {{ aspect_id }}
-    <div v-if="data !== 0" v-for="word in data" :key="word.id">
-        <div>
-            {{ word.name }}
+    <div class="word-group-box">
+        <div class="word-box">
+            <div v-if="data" v-for="word in data" :key="word.id">
+                {{ word.name }}
+            </div>
+            <div v-else>
+                Понятий нет в базе данных, либо произошла другая ошибка
+            </div>
+        </div>
+        <div class="selected-word-box">
         </div>
     </div>
-    <div v-else>
-        Понятий нет в базе данных, либо произошла другая ошибка
-    </div>
+
+
     <div>
         <NextStep :aspect_id="aspect_id"></NextStep>
     </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.word-group-box {
+    border: 10px;
+}
+</style>
