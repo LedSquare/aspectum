444ebdf86e39d5e08bae80a6a02c51f4e4d36c47
LedSquare <prophet3334@gmail.com>
2024-06-06 23:50:36 +0300

    Fea: #front - Added toogle word function and some css styles

--- laravel/resources/js/Components/Aspect/Buttons/NextStep.vue
+++ laravel/resources/js/Components/Aspect/Buttons/NextStep.vue
@@ -5,11 +5,39 @@ const props = defineProps({
 
 </script>
 <template>
-    <div>
-        <Link :href="route('aspect.post', { aspect: aspect_id })" as="button" class="start-button">
+    <div class="button-box">
+        <Link :href="route('aspect.post', { aspect: aspect_id })" as="button" class="step-button">
         Следующий шаг
         </Link>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.button-box {
+    margin-top: 0.5em;
+    margin-bottom: 0.1em;
+}
+
+.step-button {
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    font-weight: 700;
+    cursor: pointer;
+    border: 2px solid $blue;
+    color: black;
+    border-radius: 10px;
+    transition:
+        background-color 0.3s,
+        color 0.3s,
+        border 0.3s;
+
+    &:hover {
+        background-color: $blue;
+        border: 2px solid whitesmoke;
+        color: #fff;
+    }
+}
+</style>
--- laravel/resources/js/Pages/Aspect/Word.vue
+++ laravel/resources/js/Pages/Aspect/Word.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import NextStep from '../../Components/Aspect/Buttons/NextStep.vue';
+import { ref } from 'vue';
 
 const props = defineProps({
     title: String,
@@ -8,34 +9,80 @@ const props = defineProps({
     aspect_id: Number,
 });
 
+const selectedWords = ref([])
+
+function addWord(word) {
+    const index = selectedWords.value.findIndex((item) => item === word);
+    console.log(index)
+
+    if (index > -1) {
+        selectedWords.value.splice(index, 1);
+    } else {
+        selectedWords.value.push(word);
+    }
+}
 
 </script>
 <template>
 
     <Head :title="title" />
-    {{ aspect_id }}
-    <div class="word-group-box">
-        <div class="word-box">
-            <div v-if="data" v-for="word in data" :key="word.id">
-                {{ word.name }}
-            </div>
-            <div v-else>
-                Понятий нет в базе данных, либо произошла другая ошибка
-            </div>
-        </div>
-        <div class="selected-word-box">
-        </div>
-    </div>
-
-
     <div>
+        {{ aspect_id }}
+    </div>
+    <div class="aspect-frame">
+        <div style="display: flex; flex-direction: row; justify-content: center;">
+
+            <div class=" left-box">
+                <div class="word-group-box">
+                    <div class="word-box">
+                        <div class="word" @click="addWord(word)" v-if="data" v-for="word in data" :key="word.id">
+                            {{ word.name }}
+                        </div>
+                        <div v-else>
+                            Понятий нет в базе данных, либо произошла другая ошибка
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right-box">
+                <div class="selected-words">
+                    <div v-for="(word, index) in selectedWords" :key="index">
+                        {{ word.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
         <NextStep :aspect_id="aspect_id"></NextStep>
     </div>
+
+
 </template>
 
 
 <style lang="scss" scoped>
 .word-group-box {
+    .word {
+        cursor: pointer;
+        transition: 0.2s ease-in-out;
+        font-size: 18px;
+        margin: 0.2em;
+
+        &:hover {
+            font-size: 22px;
+            transition: 0.2s ease-in-out;
+        }
+    }
+
     border: 10px;
+}
+
+.selected-word-box {
+    border: 3px solid black;
+}
+
+.right-box {
+    width: auto;
+    height: auto;
+    border: 3px solid black;
 }
 </style>
