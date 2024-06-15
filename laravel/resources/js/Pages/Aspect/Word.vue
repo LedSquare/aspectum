38e527683e48fb57ce48dea0a875455bee9ec760
LedSquare <prophet3334@gmail.com>
2024-06-15 16:34:56 +0300

    Frontend: Fea: Word

--- laravel/resources/js/Pages/Aspect/Word.vue
+++ laravel/resources/js/Pages/Aspect/Word.vue
@@ -14,17 +14,15 @@ const selectedWords = ref([])
 function addWord(word) {
     const index = selectedWords.value.findIndex((item) => item === word);
 
-    if(selectedWords.value.length > 7){
+    if (selectedWords.value.length > 7) {
         console.log('Дохуя слов')
-        selectedWords.value.splice(index, 1);
+        selectedWords.value.splice(index, 1)
     } else if (index > -1) {
-        selectedWords.value.splice(index, 1);
+        selectedWords.value.splice(index, 1)
     } else {
         selectedWords.value.push(word);
     }
 }
-
-
 </script>
 <template>
 
@@ -33,17 +31,17 @@ function addWord(word) {
         {{ aspect_id }}
     </div>
     <div class="aspect-frame">
-        <div style="display: flex; flex-direction: row; justify-content: center;">
+        <div class="word-box">
 
             <div class="left-box">
-                    <div class="word" @click="addWord(word)" v-if="data" v-for="word in data" :key="word.id">
-                        {{ word.name }}
-                    </div>
-                    <div v-else>
-                        Понятий нет в базе данных, либо произошла другая ошибка
-                    </div>
+                <div class="word" @click="addWord(word)" v-if="data" v-for="word in data" :key="word.id">
+                    {{ word.name }}
+                </div>
+                <div v-else>
+                    Понятий нет в базе данных, либо произошла другая ошибка
+                </div>
             </div>
-            <div class="right-box">
+            <div class="right-box" id="selected-words-box">
                 <div class="selected-words">
                     <div @click="addWord(word)" class="word" v-for="(word, index) in selectedWords" :key="index">
                         {{ word.name }}
@@ -59,6 +57,13 @@ function addWord(word) {
 
 
 <style lang="scss" scoped>
+.word-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    justify-content: space-evenly;
+    width: 60%;
+}
 
 .left-box {
     min-width: 12em;
@@ -77,6 +82,7 @@ function addWord(word) {
     display: flex;
 
     transition: 0.2s ease-in-out;
+
     &:hover {
         font-size: 22px;
         transition: 0.2s ease-in-out;
@@ -85,8 +91,10 @@ function addWord(word) {
 
 .right-box {
     min-width: 12em;
+    max-height: 16em;
     border-radius: 15px;
     background-color: Snow;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    position: sticky;
 }
 </style>
