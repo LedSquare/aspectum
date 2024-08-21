edebb3fe58f06366552a5b131a9d86a215ba0bb4
LedSquare <prophet3334@gmail.com>
2024-08-22 01:48:38 +0300

    Fea: #Aspect  - category cycles

--- laravel/resources/js/Pages/Aspect/SelectShape.vue
+++ laravel/resources/js/Pages/Aspect/SelectShape.vue
@@ -9,43 +9,49 @@ const props = defineProps({
     aspect_id: Number,
 });
 
-const activeTab = ref(0);
-
+const activeCategory = ref(1);
+const setActiveCategory = (categoryId) => {
+    activeCategory.value = categoryId
+}
 </script>
 <template>
+
+    <Head title="Фигуры"></Head>
     <div class="aspect-frame">
-        <div v-for="shape_category in data.shape_categories">
-            <div class="shape-tab">
+        <div class="tabs-rows">
+            <div class="shape-tab" @click="setActiveCategory(shape_category.id)"
+                v-for="shape_category in data.shape_categories" :key="shape_category.id">
                 <h2>
                     {{ shape_category.name }}
                 </h2>
-                <div>
-                    <div v-for="shape in shape_category.shapes">
-                        <img class="shape" alt="some image" :src="'/' + shape.filepath">
-                    </div>
-                </div>
             </div>
+        </div>
+        <div v-for="shape in data.shape_categories.find(category => category.id === activeCategory).shapes">
+            <img class="shape" alt="some image" :src="'/' + shape.filepath">
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.tabs-rows {
+    display: flex;
+    flex-direction: row;
 
+}
 
-h2{
+h2 {
     font-size: 1.2rem;
 }
 
-.shape-tab{
+.shape-tab {
     display: flex;
     flex-direction: column;
-
-    > div{
-        display: flex;
-    }
+    padding: 0.5rem;
+    margin: 0px 2px;
 }
 
-.shape{
+
+.shape {
     width: 90;
     height: 90;
 }
