8f46db671e146b4b647ecc91e771dd5ce2dee058
LedSquare <prophet3334@gmail.com>
2024-08-22 23:57:57 +0300

    Fea: #Aspect  - change border category tab

--- laravel/resources/js/Pages/Aspect/SelectShape.vue
+++ laravel/resources/js/Pages/Aspect/SelectShape.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3';
 import NextStep from '../../Components/Aspect/Buttons/NextStep.vue';
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 const props = defineProps({
     title: String,
@@ -13,21 +13,30 @@ const activeCategory = ref(1);
 const setActiveCategory = (categoryId) => {
     activeCategory.value = categoryId
 }
+
+let activeCategoryStyle = 'border-bottom: solid 4px rgba(193, 218, 253, 1)'
+
+
 </script>
 <template>
 
     <Head title="Фигуры"></Head>
     <div class="aspect-frame">
+        <div :style="[activeCategoryStyle]">
+
+        </div>
         <div class="tabs-rows">
-            <div class="shape-tab" @click="setActiveCategory(shape_category.id)"
+            <div class="category-tab" @click="setActiveCategory(shape_category.id)"
                 v-for="shape_category in data.shape_categories" :key="shape_category.id">
-                <h2>
+                <h2 id="get-style" :style="[activeCategory === shape_category.id ? activeCategoryStyle : '']">
                     {{ shape_category.name }}
                 </h2>
             </div>
         </div>
-        <div v-for="shape in data.shape_categories.find(category => category.id === activeCategory).shapes">
-            <img class="shape" alt="some image" :src="'/' + shape.filepath">
+        <div class="grid-shapes">
+            <div v-for="shape in data.shape_categories.find(category => category.id === activeCategory).shapes">
+                <img class="shape" alt="some image" :src="'/' + shape.filepath">
+            </div>
         </div>
     </div>
 </template>
@@ -41,18 +50,27 @@ const setActiveCategory = (categoryId) => {
 
 h2 {
     font-size: 1.2rem;
+    color: $blue-gray;
+    border-bottom: solid 4px rgba($color: $blue, $alpha: 0);
+    border-radius: 0.1rem;
 }
 
-.shape-tab {
+.category-tab {
     display: flex;
     flex-direction: column;
     padding: 0.5rem;
     margin: 0px 2px;
+    cursor: pointer;
 }
-
 
 .shape {
     width: 90;
     height: 90;
+}
+
+.grid-shapes {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
 }
 </style>
