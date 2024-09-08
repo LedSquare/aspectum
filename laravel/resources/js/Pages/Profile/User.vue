8379634b177df87c9ac91b2ce95c493635e81f89
LedSquare <exilelovich@yandex.ru>
2024-09-08 22:16:47 +0300

    Fea: #Profile - added vue view

--- laravel/resources/js/Pages/Profile/User.vue
+++ laravel/resources/js/Pages/Profile/User.vue
@@ -2,6 +2,7 @@
 import { Head } from "@inertiajs/vue3";
 import { computed, ref } from "vue";
 import _ from "lodash";
+import InputGroup from "@/Components/Forms/InputGroup.vue"
 
 const props = defineProps({
     data: Array,
@@ -10,10 +11,44 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
+    <div class="profile-container">
         <Head :title="title"></Head>
-        {{ data }}
+        <div class="personal-info">
+            <header>
+                <p>
+                    Персональные данные
+                </p>
+            </header>
+            <div>
+            <input-group>
+
+            </input-group>
+                <pre>
+                    {{ data }}
+                </pre>
+            </div>
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.personal-info{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+header{
+    width: 100%;
+    border-bottom: 2px solid $blue-gray;
+    margin-bottom: 10px;
+    p{
+        margin: 1rem;
+    }
+}
+.profile-container{
+    width: 100%;
+    min-height: 900px;
+    box-sizing: border-box;
+}
+</style>
