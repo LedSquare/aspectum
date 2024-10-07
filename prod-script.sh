77ca9ba9de0853c39b8ce4f176cc705d2e316aab
LedSquare <exilelovich@yandex.ru>
2024-10-07 14:49:32 +0300

    Fix: prod-script

--- prod-script.sh
+++ prod-script.sh
@@ -4,9 +4,11 @@ apt update -y && apt upgrade -y
 
 apt install -y \
     php8.2 \
-    php8.2-{fpm,cli,bz2,curl,mbstring,intl,xml} \
+    php8.2-{fpm,cli,bz2,curl,mbstring,intl,xml,zip} \
     nginx \
-    postgresql
+    postgresql \
+    git
+
 
 a2enconf php8.2-fpm
 systemctl disable apache2
