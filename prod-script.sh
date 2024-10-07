7a3c310dd7b694f715bb6c599c094c572c091f14
LedSquare <exilelovich@yandex.ru>
2024-10-07 17:07:10 +0300

    Fea: Configurate deploy without docker environment

--- prod-script.sh
+++ prod-script.sh
@@ -3,16 +3,20 @@ echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selecti
 apt update -y && apt upgrade -y 
 
 apt install -y \
-    php8.2 \
-    php8.2-{fpm,cli,bz2,curl,mbstring,intl,xml,zip} \
-    nginx \
     postgresql \
-    git
-
+    php8.2 \
+    php8.2-{fpm,cli,bz2,curl,mbstring,intl,xml,bcmatch,pgsql} \
+    nginx \
+    git 
 
 a2enconf php8.2-fpm
 systemctl disable apache2
 
+cp ./deploy_local/default /etc/nginx/sites-available/default  
+cp ./deploy_local/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
+
+systemctl restart nginx 
+systemctl restart postgresql
 
 rm -rf /var/lib/apt/lists/*
 
